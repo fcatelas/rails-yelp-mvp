3b2c0a96295acf3ccf135ae9e1db392b8818aca9
francois catelas <catelas.francois@gmail.com>
2024-11-13 17:09:00 +0100

    yelp

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,15 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require 'faker'
+puts 'Creating 5 fake restaurants...'
+5.times do
+  restaurant = Restaurant.create(
+    name:Faker::Restaurant.name,
+    address:Faker::Address.street_address,
+    phone_number:Faker::PhoneNumber.phone_number,
+    category: ["chinese", "italian", "japanese", "french", "belgian"].sample
+  )
+  restaurant.save!
+end
+puts 'Finished'
